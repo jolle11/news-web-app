0b56e5b0a4631587482633a6a16b2bb9d9ef3a41
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-07-07 20:50:40 +0200

    Post now working

--- routes/newsApi.js
+++ routes/newsApi.js
@@ -10,8 +10,6 @@ router.get(
 				"http://newsapi.org/v2/everything?" + "q=tech&" + "apiKey=2c6bfa81c2e8403da6eff5d85b8d1432";
 
 			const news_get = await axios.get(url);
-			// console.log(res);
-			// console.log("news", { articles: news_get.data.articles });
 			res.json({ articles: news_get.data.articles });
 		} catch (error) {
 			if (error.response) {
@@ -24,13 +22,13 @@ router.get(
 router.post(
 	"/search",
 	async (req, res) => {
-		const search = req.body.search;
-		// console.log(req.body.search)
+		const search = req.query.q;
+		console.log(req.query.q);
 		try {
 			let url = `http://newsapi.org/v2/everything?q=${search}&apiKey=579b3659478549c5a876913518ba3904`;
 
 			const news_get = await axios.get(url);
-			res.render("news", { articles: news_get.data.articles });
+			res.json({ articles: news_get.data.articles });
 		} catch (error) {
 			if (error.response) {
 				console.log(error);
